fa287e4e458556393f5a655354eb887fcc0240cc
Nicholas Le <nle031@west-mec.org>
2019-02-27 17:25:40 -0700

    Final Commit
    Added the code to display and calculate the total of each item.
    Ready for review.

--- tc_cart.js
+++ tc_cart.js
@@ -11,3 +11,22 @@
    Filename: tc_cart.js
 	
 */
+//Displays default cart order cost
+var orderTotal = 0;
+
+//HTML code for the contents of the shopping cart in table form
+var cartHTML = "<table> <tr> <th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th></tr>";
+
+//Displays the price, item, description and quantity for the cart
+for (var i = 0; i <= 3; i++) {
+    cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png' alt='" + item[i] + "' /></td>";
+    cartHTML += "<td>" + itemDescription[i] + "</td> <td>$" + itemPrice[i] + "</td> <td>" + itemQty[i] + "</td>";
+
+    //Multiplys the quantity to display the correct total.
+    var itemCost = itemPrice[i] * itemQty[i];
+    cartHTML += "<td>$" + itemCost + "</td></tr>";
+    orderTotal = orderTotal + itemCost;
+}
+cartHTML += "<tr> <td colspan='4'>Subtotal</td> <td>" + orderTotal + "</td> </tr> </table>";
+//Displays the cart
+document.getElementById("cart").innerHTML = cartHTML;
